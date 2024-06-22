d52cf8b36ac4bb3a9e42fef2c6571d96e353c58b
nasim-etlimited <nasim@etlimited.net>
2024-06-22 15:22:20 +0600

    concise code

--- index.js
+++ index.js
@@ -22,12 +22,10 @@ app.get('/api/courses/', (req, res) => {
 
 app.post('/api/courses/', (req, res) => {
     const { error } = validateCourse(req.body); // result.error "object distructure"
-    if (error) {
-        // 400 Bad Request
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-
+    // 400 Bad Request
+    if (error) return res.status(400).send(result.error.details[0].message);
+      
+    // ********* manual logic
     // const schema = {
     //     name : Joi.string().min(3).required()
     // };
@@ -41,6 +39,7 @@ app.post('/api/courses/', (req, res) => {
     //     return;
     // }
 
+    // ************manual validation
     // if (!req.body.name || req.body.name.length < 3) {
     //     // 400 Bad Request
     //     res.status(400).send('Name is required & should be minimum 3 character');
@@ -60,19 +59,16 @@ app.put('/api/courses/:id', (req, res) =>{
     // loop up the course
     // if not existing, return 404
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('This course with the given ID was not found.');
+    if (!course) return res.status(404).send('This course with the given ID was not found.');
 
 
     // validate
     // if invalid, return 400 - bad request
 
     const { error } = validateCourse(req.body); // result.error
-    if (error) {
-        // 400 Bad Request
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
- 
+    // 400 Bad Request
+    if (error) return res.status(400).send(result.error.details[0].message);
+      
     // Update course
     // return the updated course
     course.name = req.body.name;
@@ -89,9 +85,27 @@ function validateCourse(course) {
 }
 
 
+app.delete('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // Not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('This course with the given ID was not found.');
+
+
+    // Delete 
+    const index = courses.indexOf(course);
+    courses.splice(index, 1); 
+
+    // Return the same course
+    res.send(course);
+});
+
+
+
+
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('This course with the given ID was not found.');
+    if (!course) return res.status(404).send('This course with the given ID was not found.');
     res.send(course);
 });
 
