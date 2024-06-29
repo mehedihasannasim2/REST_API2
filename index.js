4ce7f5bfc05612a2aefced5a66d967942efa97ed
nasim-etlimited <nasim@etlimited.net>
2024-06-29 14:19:43 +0600

    Structuring Express Application

--- index.js
+++ index.js
@@ -9,19 +9,14 @@ const morgan = require('morgan');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
-const logger = require('./logger');
+const logger = require('./middleware/logger');
+
+const courses = require('./routes/courses'); 
+const home = require('./routes/home'); 
 
 // Templating engine
 app.set('view engine', 'pug');
 app.set('views', './views');  // default
-
-
-
-// console.log(`Node_ENV: ${process.env.NODE_ENV}`);
-// console.log(`app: ${app.get('env')}`);
-// app.get('env')
-// process.env.NODE_ENV  // undefined 
-
 
 // Configuration
 console.log("Appliction name: " + config.get('name'));
@@ -36,6 +31,10 @@ app.use(express.static('public')); // key=value&key=value
 
 // Third party middleware
 app.use(helmet()); 
+
+app.use('/api/courses', courses);
+app.use('/', home);
+
 
 if (app.get('env') === 'development'){
     app.use(morgan('tiny'));    
@@ -53,112 +52,6 @@ app.use(function(req, res, next) {
     next();
 })
 
-
-
-const courses = [
-    { id: 1, name: 'course1'},
-    { id: 2, name: 'course2'},
-    { id: 3, name: 'course3'},
-]
-
-app.get('/', (req, res) => {
-    res.render('index', {title: 'My Express App', message: 'hello nasim' });
-});
-
-
-// app.get('/api/courses/', (req, res) => {
-//     res.send(courses);
-// });
-
-
-app.post('/api/courses/', (req, res) => {
-    const { error } = validateCourse(req.body); // result.error "object distructure"
-    // 400 Bad Request
-    if (error) return res.status(400).send(result.error.details[0].message);
-      
-    // ********* manual logic
-    // const schema = {
-    //     name : Joi.string().min(3).required()
-    // };
-
-    // const result = Joi.validate(req.body, schema);
-    // // console.log(result);
-
-    // if (result.error) {
-    //     // 400 Bad Request
-    //     res.status(400).send(result.error.details[0].message);
-    //     return;
-    // }
-
-    // ************manual validation
-    // if (!req.body.name || req.body.name.length < 3) {
-    //     // 400 Bad Request
-    //     res.status(400).send('Name is required & should be minimum 3 character');
-    //     return;
-    // }
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name 
-    };
-    courses.push(course);
-    res.send(course);
-});
-
-
-app.put('/api/courses/:id', (req, res) =>{
-    // loop up the course
-    // if not existing, return 404
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('This course with the given ID was not found.');
-
-
-    // validate
-    // if invalid, return 400 - bad request
-
-    const { error } = validateCourse(req.body); // result.error
-    // 400 Bad Request
-    if (error) return res.status(400).send(result.error.details[0].message);
-      
-    // Update course
-    // return the updated course
-    course.name = req.body.name;
-    res.send(course)
-
-});
-
-function validateCourse(course) {
-    const schema = {
-        name : Joi.string().min(3).required()
-    };
-
-    return Joi.validate(course, schema);
-}
-
-
-app.delete('/api/courses/:id', (req, res) => {
-    // Look up the course
-    // Not existing, return 404
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('This course with the given ID was not found.');
-
-
-    // Delete 
-    const index = courses.indexOf(course);
-    courses.splice(index, 1); 
-
-    // Return the same course
-    res.send(course);
-});
-
-
-
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('This course with the given ID was not found.');
-    res.send(course);
-});
 
 
 // PORT
